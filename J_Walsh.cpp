0f4c1630fcf7a7929967dbe463478e771de2173b
terror <kanishkverma3688@gmail.com>
2018-10-17 11:18:14 +0530

    Update J_Walsh.cpp

--- J_Walsh.cpp
+++ J_Walsh.cpp
@@ -1,10 +1,10 @@
 //julie walsh 
 #include <iostream>
-
+#include<stdio.h>
 using namespace std; 
-
 int main(int argc, char*argv[])
 {
 	cout << "Hello World!!" << endl;
+	printf("Hello World");
 	return 0;
 }
